6a067b3e7a5c5f087c02c2ebd573cde483aef0ac
Chris Wilson <christopher.e.wilson@gmail.com>
2013-04-06 14:58:07 -0400

    small update

--- scripts/pie.py
+++ scripts/pie.py
@@ -10,22 +10,20 @@ class PIE_Parser:
     def parse(self, classname, pattern, text):
         #first, get every part of the text that is not marked up
         #that is, by default, text outside the <div> tags that we add as we find matches
-        print classname, pattern        
-        print "\n----\n".join(re.findall(self.pattern, text, re.S | re.I))
+        print "==================>", classname, pattern
+        print "\n----\n".join(re.findall(self.pattern, text, re.S))
         
-        return re.sub(self.pattern, self._sub_regex(classname, pattern), text, re.S | re.I)
+        return re.sub(self.pattern, self._sub_regex(classname, pattern), text, re.S)
 
     #receives each region of text not yet matched and runs the real regex against it
     def _sub_regex(self, name, pattern):
-        print name, pattern
         def repl(match):
-            return match.group(0).replace(match.group(1), re.sub(pattern, self._markup(name), match.group(1), re.S | re.I))
+            return match.group(0).replace(match.group(1), re.sub(pattern, self._markup(name), match.group(1), re.S))
         return repl
         
     #take matches of the real regex pattern and marks them up with tags
     def _markup(self, name):
         def repl(match):
-            #print match.group(0)
             #some patterns have submatches, some don't
             if len(match.groups()) == 0:
                 print match.group(0) 
--- scripts/read.py
+++ scripts/read.py
@@ -13,10 +13,10 @@ websters = re.sub(" +", " ", websters)
 websters = re.sub("[\r\n]+", "  ", websters)
 
 definitions = [
-    ("word", r"\s{2}([[A-Z][A-Z\s-]*)\s{2}"),
+    ("word", r"\s{2}([A-Z][A-Z\s-]*)\s{2}"),
     ("origin", r"\([A-z]{2,6}\.\)"),
     ("etymology", r"\bEtym: \[(.*?)\]"),
-    ("source", r"(?:\.|\])\s([A-Z][A-z]{3,15}\.)\s\s"),
+    ("source", r"(?:\]|\.)\s([A-Z][A-z]{3,15}\.)\s\s"),
     ("definition", r"\d{1,2}\. (.+?)(?:\d|\s{2})"),
     ("definition", r"Defn: (.+)")
 ]
@@ -30,12 +30,8 @@ def sample(text, margin=5000):
 
 t = sample(websters, 2000)
 
-
 for c in range(len(definitions)):
     t = PIE.parse(definitions[c][0], definitions[c][1], t)
-
-
-t = t.replace("\n", "")
 
 f = open(os.getcwd() + "/data/test.json", 'w')
 f.write(json.dumps({ "text": t}))
